11e12d17046db489b208080812ae9ed784464150
PedroOttavio <pedroottavioss@gmail.com>
2024-11-26 10:33:35 -0300

    configurando algumas coisas no voluntarios, slide django5, page 16

--- voluntarios/views.py
+++ voluntarios/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.core.paginator import Paginator
+from django.contrib import messages
 
 from .models import Voluntario
 from .forms import VoluntarioModelForm
@@ -14,23 +17,35 @@ class VoluntariosView(ListView):
         if buscar:
             return qs.filter(nome__icontains=buscar)
         return qs
+    
+        if qs.count() > 0:
+            paginator = Paginator(qs, 1)
+            listagem = paginator.get_page(self.request.GET.get('page'))
+            return listagem
+        else:
+            return messages.info(self.request, 'Nenhum voluntário encontrado.')
+        
 
-
-class VoluntarioAddView(CreateView):
+class VoluntarioAddView(SuccessMessageMixin ,CreateView):
     model = Voluntario
     form_class = VoluntarioModelForm
     template_name = 'voluntario_form.html'
     success_url = reverse_lazy('voluntarios')
+    success_message = 'Voluntário cadastrado com sucesso.'
     
     
-class VoluntarioUpdateView(UpdateView):
+class VoluntarioUpdateView(SuccessMessageMixin, UpdateView):
     model = Voluntario
     form_class = VoluntarioModelForm
     template_name = 'voluntario_form.html'
     success_url = reverse_lazy('voluntarios')
+    success_message = 'Voluntário atualizado com sucesso.'
     
     
-class VoluntarioDeleteView(DeleteView):
+class VoluntarioDeleteView(SuccessMessageMixin, DeleteView):
     model = Voluntario
     template_name = 'voluntario_apagar.html'
     success_url = reverse_lazy('voluntarios')
+    success_message = 'Voluntário apagado com sucesso.'
+    
+    
